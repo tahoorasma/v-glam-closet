6c074403b57b84d419ea4647f8f5f4388e34e546
Tahoor Asma <102382100+tahoorasma@users.noreply.github.com>
2025-03-12 23:08:50 +0500

    accessoryCatalog

--- backend/app.py
+++ backend/app.py
@@ -15,14 +15,15 @@ collection = db["Product"]
 @app.route('/makeupCatalog', methods=['GET'])
 def get_items():
     makeup_subcategories = db["SubCategory"].find({"categoryID": "C001"}, {"subCategoryID": 1, "_id": 0})
-    
-    # Extract subCategoryIDs from query result
     makeup_subcategory_ids = [sub["subCategoryID"] for sub in makeup_subcategories]
-
-    # Fetch only products that belong to the retrieved subcategories
     makeup_products = list(db["Product"].find({"subCategoryID": {"$in": makeup_subcategory_ids}}, {"_id": 0}))
-
     return jsonify(makeup_products)
+
+@app.route('/accessoryCatalog', methods=['GET'])
+def get_accessory_products():
+    subcategory = request.args.get('subcategory', 'jewelry')  # Default to Jewelry
+    accessory_products = list(db["Product"].find({"subCategoryID": subcategory}, {"_id": 0}))
+    return jsonify(accessory_products)
 
 if __name__ == '__main__':
     app.run(debug=True)
